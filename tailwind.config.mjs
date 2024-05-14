08c517a1b5d94df22a99ece504874e4d11d41dbb
Mrsty21 <riverasoto.marcelo@gmail.com>
2024-05-13 19:24:57 -0600

    second commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -11,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
